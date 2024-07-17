982835b7ba8f53728d08769412275631e2041fb4
Joe Clark <weirdlookingjay@live.com>
2024-07-16 21:55:51 -0400

    Created Header & Sidebar Skeleton v2

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,23 +10,29 @@ import {
 import { MenuIcon } from "lucide-react";
 
 function Sidebar() {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+    </>
+  );
+
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
-      <Sheet>
-        <SheetTrigger>
-          <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
-        </SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader>
-            <SheetTitle>Menu</SheetTitle>
-            <div></div>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-
-      <div className="hidden md:inline">
-        <NewDocumentButton />
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>{menuOptions}</SheetTitle>
+              <div></div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
+
+      <div className="hidden md:inline">{menuOptions}</div>
     </div>
   );
 }
